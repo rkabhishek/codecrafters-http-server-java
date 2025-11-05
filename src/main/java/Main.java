27d62a7b5684ebe74db0e6365924c0e5a009db2b
Abhishek Kaushik <akaushik079@gmail.com>
2025-11-05 13:58:31 +0530

    Refactor code for readability (extract constants, guard checks)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,15 +5,17 @@ import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
 public class Main {
+    private static final String CRLF = "\r\n";
+    private static final String STATUS_OK = "HTTP/1.1 200 OK";
+    private static final String STATUS_BAD_REQUEST = "HTTP/1.1 400 Bad Request";
+    private static final String STATUS_NOT_FOUND = "HTTP/1.1 404 Not Found";
     private static final String MISSING_HEADER = "Missing User-Agent header";
+    private static final String HEADER_TEMPLATE = "Content-Type: text/plain%sContent-Length: %d%s";
   public static void main(String[] args) {
-      // You can use print statements as follows for debugging, they'll be visible when running tests.
-      System.out.println("Logs from your program will appear here!");
-
+      System.out.println("Logs from the program will appear here!");
      try {
        ServerSocket serverSocket = new ServerSocket(4221);
-
-       // Since the tester restarts your program quite often, setting SO_REUSEADDR
+       // Since the tester restarts the program quite often, setting SO_REUSEADDR
        // ensures that we don't run into 'Address already in use' errors
        serverSocket.setReuseAddress(true);
 
@@ -37,15 +39,16 @@ public class Main {
   }
 
   static String createResponse(String requestLine, BufferedReader reader) throws IOException {
-      String urlPath = requestLine.split(" ")[1];
+      String[] parts = requestLine.split(" ");
+      String urlPath = parts.length > 1 ? parts[1] : "/";
       if (Objects.equals(urlPath, "/")) {
-          return "HTTP/1.1 200 OK\r\n\r\n";
+          return String.format("%s%s%s", STATUS_OK, CRLF, CRLF);
       } else if (urlPath.startsWith("/echo")) {
           return handleEchoRequest(urlPath);
       } else if (urlPath.startsWith("/user-agent")) {
           return handleUserAgentRequest(reader);
       } else {
-          return "HTTP/1.1 404 Not Found\r\n\r\n";
+          return String.format("%s%s%s", STATUS_NOT_FOUND, CRLF, CRLF);
       }
   }
 
@@ -56,8 +59,8 @@ public class Main {
           body = path.substring(prefix.length());
       }
 
-      String headers = String.format("Content-Type: text/plain\r\nContent-Length: %d\r\n", body.length());
-      return String.format("HTTP/1.1 200 OK\r\n%s\r\n%s", headers, body);
+      String headers = String.format(HEADER_TEMPLATE, CRLF, body.length(), CRLF);
+      return String.format("%s%s%s%s%s", STATUS_OK, CRLF, headers, CRLF, body);
   }
 
   private static String handleUserAgentRequest(BufferedReader bufferedReader) throws IOException {
@@ -68,10 +71,10 @@ public class Main {
               System.out.println("inside prefix\n");
               String body = line.split(" ")[1];
               System.out.println("body is " + body);
-              String headers = String.format("Content-Type: text/plain\r\nContent-Length: %d\r\n", body.length());
-              return String.format("HTTP/1.1 200 OK\r\n%s\r\n%s", headers, body);
+              String headers = String.format(HEADER_TEMPLATE, CRLF, body.length(), CRLF);
+              return String.format("%s%s%s%s%s", STATUS_OK, CRLF, headers, CRLF, body);
           }
       }
-      return String.format("HTTP/1.1 400 Bad Request\r\n%s\r\n%s", MISSING_HEADER, MISSING_HEADER.length());
+      return String.format("%s%s%s%s%s", STATUS_BAD_REQUEST, CRLF, MISSING_HEADER, CRLF, MISSING_HEADER.length());
   }
 }
