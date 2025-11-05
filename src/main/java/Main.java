cb113b28f2dffa9959dccd6b617b7f069be237a3
Abhishek Kaushik <akaushik079@gmail.com>
2025-11-06 01:16:33 +0530

    Handle returning file contents

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Objects;
 
 public class Main {
@@ -11,8 +13,20 @@ public class Main {
     private static final String STATUS_NOT_FOUND = "HTTP/1.1 404 Not Found";
     private static final String MISSING_HEADER = "Missing User-Agent header";
     private static final String HEADER_TEMPLATE = "Content-Type: text/plain%sContent-Length: %d%s";
+    private static final String HEADER_CONTENT_TYPE = "Content-Type";
+    private static final String TYPE_OCTET_STREAM = "application/octet-stream";
+    private static final String HEADER_CONTENT_LENGTH = "Content-Length";
+    private static String baseDirectory;
+
   public static void main(String[] args) {
       System.out.println("Logs from the program will appear here!");
+
+      if (args.length == 2) {
+           if (Objects.equals(args[0], "--directory")) {
+               baseDirectory = args[1];
+           }
+      }
+
       try (ServerSocket serverSocket = new ServerSocket(4221)) {
           // Since the tester restarts the program quite often, setting SO_REUSEADDR
           // ensures that we don't run into 'Address already in use' errors
@@ -51,6 +65,8 @@ public class Main {
           return handleEchoRequest(urlPath);
       } else if (urlPath.startsWith("/user-agent")) {
           return handleUserAgentRequest(reader);
+      } else if (urlPath.startsWith("/files")) {
+          return handleFileRequest(urlPath);
       } else {
           return String.format("%s%s%s", STATUS_NOT_FOUND, CRLF, CRLF);
       }
@@ -80,5 +96,26 @@ public class Main {
           }
       }
       return String.format("%s%s%s%s%s", STATUS_BAD_REQUEST, CRLF, MISSING_HEADER, CRLF, MISSING_HEADER.length());
+  }
+
+  private static String handleFileRequest(String urlPath) throws IOException {
+      System.out.println("handling file");
+      String pathPrefix = "/files/";
+      String fileName = urlPath.substring(pathPrefix.length());
+      if (fileName.isEmpty()) {
+          return String.format("%s%s%s", STATUS_NOT_FOUND, CRLF, CRLF);
+      }
+
+      String fullPath =  baseDirectory + fileName;
+      Path path = Path.of(fullPath);
+      if (!Files.exists(path)) {
+          return String.format("%s%s%s", STATUS_NOT_FOUND, CRLF, CRLF);
+      }
+
+      String content = new String(Files.readAllBytes(path));
+      int contentLength = content.length();
+
+      String headers = String.format("%s: %s%s%s: %s%s", HEADER_CONTENT_TYPE, TYPE_OCTET_STREAM, CRLF, HEADER_CONTENT_LENGTH, contentLength, CRLF);
+      return String.format("%s%s%s%s%s", STATUS_OK, CRLF, headers, CRLF, content);
   }
 }
